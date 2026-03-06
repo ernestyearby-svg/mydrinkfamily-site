0cdec13e5d159d9c58b6559b05c4ea6791d516da
ernestyearby-svg <ernestyearby@gmail.com>
2026-03-06 14:17:54 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
-import Header from "./components/Header";
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+title: "My Drink Family",
+description: "The Original Mimosa in a Can.",
+};
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>
-        <Header />
-        {children}
-      </body>
-    </html>
-  );
+children,
+}: Readonly<{
+children: React.ReactNode;
+}>) {
+return (
+<html lang="en">
+<body>{children}</body>
+</html>
+);
 }
